82a1a30058c2dff1092c62fc8ede005d40b75532
Andrés Javier López <ajavier.lopez@gmail.com>
2012-06-20 15:19:37 -0600

    se documenta el módulo restobject

--- pyros/restobject.py
+++ pyros/restobject.py
@@ -10,63 +10,80 @@ import web
 import json
 
 class RestObject(object):
+    '''Prototipo de un objeto REST para construir un nodo en el API, se deben de implementar sus procesos'''
     def GET(self, element=None):
+        '''Devuelve la respuesta al método GET del protocolo HTTP'''
         if(element is None or element == '/'):
             return self._response(self.read())
         else:
             return self._response(self.getElement(self._prepareId(element)))
     
     def POST(self, element=None):
+        '''Devuelve la respuesta al método POST del protocolo HTTP'''
         if(element is None or element == '/'):
             return self._response(self.insert())
         else:
             return self._response(self.insertInto(self._prepareId(element)))
     
     def PUT(self, element=None):
+        '''Devuelve la respuesta al método PUT del protocolo HTTP'''
         if(element is None or element == '/'):
             return self._response(self.replace())
         else:
             return self._response(self.updateElement(self._prepareId(element)))
     
     def DELETE(self, element=None):
+        '''Devuelve la respuesta al método DELETE del protocolo HTTP'''
         if(element is None or element == '/'):
             return self._response(self.delete())
         else:
             return self._response(self.deleteElement(self._prepareId(element)))
     
     def _response(self, data):
+        '''Convierte el diccionario proporcionado en una respuesta del tipo JSON'''
         web.header('Content-Type', 'application/json')
         return json.dumps(data)
     
     def _prepareId(self, string):
+        '''Procesa el parametro opcional obtenido para que se utilice como id'''
         return string.replace('/', '')
     
     def _respSuccess(self, result):
+        '''Respuesta estándar para casos de éxito o fallo de un proceso'''
         return self._resp('success', result)
     
     def _resp(self, tag, data):
+        '''Le da el formato adecuado a la respuesta que devuelven los métodos'''
         return {tag: data}
     
     def read(self):
+        '''Devuelve una lista de elementos en el nodo actual'''
         pass
     
     def insert(self):
+        '''Ingresa un nuevo elemento al nodo actual'''
         pass
     
     def replace(self):
+        '''Reemplaza completamente el nodo actual'''
         pass
     
     def delete(self):
+        '''Elimina completamente el nodo actual'''
         pass
     
     def getElement(self, id_element):
+        '''Devuelve el elemento específicado perteneciente al nodo actual'''
         pass
     
     def insertInto(self, id_element):
+        '''Ingresa un subelemento al elemento especificado dentro del nodo'''
         pass
     
     def updateElement(self, id_element):
+        '''Actualiza el elemento específicado en el nodo actual'''
         pass
     
     def deleteElement(self, id_element):
+        '''Elimina el elemento específicado dentro del nodo'''
         pass
