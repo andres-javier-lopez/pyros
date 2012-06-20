e4daa39283564f8ee94be7632a8a1733084cb6ff
Andrés Javier López <ajavier.lopez@gmail.com>
2012-06-20 15:58:03 -0600

    Agregados comentarios a módulo urlmapper

--- pyros/urlmapper.py
+++ pyros/urlmapper.py
@@ -8,12 +8,16 @@ Created on 20/06/2012
 '''
 
 class URL(object):
+    '''Objeto para la construcción de las URL de los nodos del API'''
     def __init__(self):
+        '''Inicializa el mapa de rutas como vacío'''
         self.routemap = ()
         
     def add(self, route, handler):
+        '''Agrega un nuevo par ruta/manejador al mapa de rutas'''
         self.routemap = self.routemap + (route + '(/\d*)?', handler)
     
     def getMap(self):
+        '''Devuelve el mapa de rutas'''
         return self.routemap
         
