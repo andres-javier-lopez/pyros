7bfdb40e6716cde1860cb9a8557791a84b2917bf
Andrés Javier López <ajavier.lopez@gmail.com>
2013-02-20 16:11:00 -0600

    Nuevo funcionamiento para método get basado en decorators

--- pyros/restobject.py
+++ pyros/restobject.py
@@ -8,51 +8,62 @@ import web
 import json
 import traceback
 import auth
+from decorations import base_decorator
+import inspect
 
 debug_info = False
 
+@base_decorator
+def get(f):
+    def func(*args, **kwargs):
+        return f(*args, **kwargs)
+    func.method = 'get'
+    func.type = '_default'
+    return func
+
+def getlist(type):
+    @base_decorator
+    def sub(f):
+        def func(*args, **kwargs):
+            return f(*args, **kwargs)
+        func.method = 'get'
+        func.type = type
+        return func
+    return sub
+
+@base_decorator
+def getall(f):
+    def func(element, *args, **kwargs):
+        return f(*args, **kwargs)
+    func.method = 'get'
+    func.type = '_all'
+    return func
+
+
 class RestObject(object):
     u"""Prototipo de un objeto REST para construir un nodo en el API, se deben de implementar sus procesos"""
-    def authenticate(self, method):
-        data = web.input()
-        try:
-            signature = data.signature
-            timestamp = data.timestamp
-        except AttributeError:
-            if(self.get_auth_key(method) == ''):
-                return
-            else:
-                raise auth.AuthError()
-            
-        authobj = auth.Auth(self.get_auth_key(method))
-        
-        datastring = method + ' ' + web.ctx.path
-        sep = '?'
-        for key in sorted(data.iterkeys()):
-            if(key != 'signature'):
-                datastring +=  sep + key + '=' + data[key]
-                if(sep == '?'):
-                    sep = '&'
-        
-        if(not authobj.is_valid(datastring, signature, timestamp)):
-            raise auth.AuthError()
+    def __init__(self):
+        self.get_functions = {}
+        for func in inspect.getmembers(self, inspect.ismethod):
+            try:
+                if func[1].method == 'get':
+                    self.get_functions[func[1].type] = func[1]
+            except AttributeError:
+                pass 
     
-    def get_auth_key(self, method):
-        return ''
-    
-    def GET(self, element=None):
+    def GET(self, element=None, type=None):
         u"""Devuelve la respuesta al método GET del protocolo HTTP"""
-        try:
-            self.authenticate('GET')
+        if(type is None):
             if(element is None or element == '/'):
-                return self._response(self.read())
+                func = self.get_functions['_all']
             else:
-                return self._response(self.get_element(self._prepare_id(element)))
-        except auth.AuthError as e:
-            web.webapi.unauthorized()
-            return self._response(self._resp_error(e.__str__(), str(type(e))))
-        except Exception as e:
-            return self._response(self._resp_error(e.__str__(), str(type(e))))
+                func = self.get_functions['_default']
+        else:
+            try:
+                func = self.get_functions[self._prepare_id(type)]
+            except KeyError:
+                return # Aquí debe haber un error 404
+        return func(self._prepare_id(element))
     
     def POST(self, element=None):
         u"""Devuelve la respuesta al método POST del protocolo HTTP"""
@@ -103,7 +114,10 @@ class RestObject(object):
     
     def _prepare_id(self, string):
         u"""Procesa el parametro opcional obtenido para que se utilice como id"""
-        return string.replace('/', '')
+        if string is not None:
+            return string.replace('/', '')
+        else:
+            return string
     
     def _resp(self, tag, data):
         u"""Le da el formato adecuado a la respuesta que devuelven los métodos"""
