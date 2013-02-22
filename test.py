4696279cdab7cdbd6d1b6e24ddf63eaa9e392caf
Andrés Javier López <ajavier.lopez@gmail.com>
2013-02-22 00:07:46 -0600

    ampliada la prueba para todos los tipos actuales de request

--- test.py
+++ test.py
@@ -2,27 +2,37 @@
 
 import pycurl
 import StringIO
+import random
+import string
 
 request = "http://localhost:8080/try/"
 
 class Test(object):
-    def run(self):
-        self.init_curl()
-        methods = ['GET', 'POST', 'PUT', 'DELETE']
-        for method in methods:
-            b = StringIO.StringIO()
-            self.c.setopt(pycurl.WRITEFUNCTION, b.write)
-            self.c.setopt(pycurl.CUSTOMREQUEST, method)
-            self.c.perform()
-            print b.getvalue()
-            b.close()
-        
-    def init_curl(self):
+    def __init__(self):
         self.c = pycurl.Curl()
         self.c.setopt(pycurl.URL, request)
         self.c.setopt(pycurl.FOLLOWLOCATION, 1)
         self.c.setopt(pycurl.MAXREDIRS, 5)
-
+    
+    def run(self):
+        self.restobject_test()
+        
+    def restobject_test(self):
+        rand = ''.join(random.choice(string.lowercase + string.digits) for i in range(4))
+        req = ['', rand, rand + '/valores']
+        for r in req:
+            self.set_request(request + r)
+            methods = ['GET', 'POST', 'PUT', 'DELETE']
+            for method in methods:
+                b = StringIO.StringIO()
+                self.c.setopt(pycurl.WRITEFUNCTION, b.write)
+                self.c.setopt(pycurl.CUSTOMREQUEST, method)
+                self.c.perform()
+                print b.getvalue()
+                b.close()
+        
+    def set_request(self, request):
+        self.c.setopt(pycurl.URL, request)
 
 if __name__ == '__main__':
     test = Test()
