38751d811ef05e2822ac6ed6d577fd37fb0266f9
Andrés Javier López <ajavier.lopez@gmail.com>
2013-02-27 10:43:42 -0600

    Mejoras al sistema de pruebas

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import random
 import string
 import time
 import hashlib, hmac
+import json
 
 url = "http://localhost:8080/"
 auth_key = "1234"
@@ -20,24 +21,34 @@ class Test(object):
     def run(self):
         self.restobject_test()
         self.auth_test()
+        self.database_test()
         
     def restobject_test(self):
         rand = ''.join(random.choice(string.lowercase + string.digits) for i in range(4))
         req = ['', rand, rand + '/valores']
         for r in req:
-            self._set_request(url + 'basic/' + r)
             for method in self.methods:
-                self.c.setopt(pycurl.CUSTOMREQUEST, method)
-                self._print_results()
+                self._make_request('basic/' + r, method)
     
     def auth_test(self):
         for method in self.methods:
             timestamp = str(int(time.time()))
             datastring = method + " /auth/?timestamp=" + timestamp
             hash = hmac.new(auth_key, datastring, hashlib.sha256).hexdigest()
-            self._set_request(url + 'auth/?timestamp=' + timestamp + '&signature=' + hash)
-            self.c.setopt(pycurl.CUSTOMREQUEST, method)
-            self._print_results()
+            self._make_request('auth/?timestamp=' + timestamp + '&signature=' + hash, method)
+    
+    def database_test(self):
+        data = json.dumps({"valor1": "test", "valor2": "5" })
+        self._make_request('test1', 'POST', body=data)
+    
+    def _make_request(self, url_string, method, body = None):
+        self._set_request(url + url_string)
+        self.c.setopt(pycurl.CUSTOMREQUEST, method)
+        if(body is not None):
+            self.c.setopt(pycurl.POSTFIELDS, body)
+        else:
+            self.c.setopt(pycurl.POSTFIELDS, "")
+        self._print_results()
         
     def _set_request(self, request):
         self.c.setopt(pycurl.URL, request)
