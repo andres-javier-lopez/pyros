ff35f971ae4f801ee977cd3b10deafe8d2fa48b8
Andrés Javier López <ajavier.lopez@gmail.com>
2013-04-02 10:59:32 -0600

    Agregada configuracion de aplicación y eliminado super de urlmapper

--- app/config.py
+++ app/config.py
@@ -1,4 +1,28 @@
 #coding: utf-8
 
-urls = ()
-debug = True
+'''
+@author: Andrés Javier López <ajavier.lopez@gmail.com>
+@version: 
+'''
+import pyros.database
+import pyros.urlmapper
+
+urlmap = pyros.urlmapper.URL()
+## Configuradas las rutas de la aplicación
+## urlmap.add('/', 'test.simple.Start')
+urls = urlmap.get_map()
+
+## Conexión con la base de datos
+database = {'dbn': 'mysql', 'user': 'root', 'password': '', 'database': 'test'}
+
+## Depuración desactivada para producción
+debug = False
+
+def init_connection():
+    pyros.database.Database.initialize(database)
+    
+def check_database():
+    if(pyros.database.check_connection() == False):
+        init_connection()
+    
+init_connection()
